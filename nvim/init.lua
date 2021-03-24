9fcbfb1affe4fd4c534d42ea5c01b8eaa6353a26
Alberto Maturano <alberto@maturano.mx>
2021-03-24 11:50:12 -0600

    [nvim] General settings

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,6 @@
 local execute = vim.api.nvim_command
 local fn = vim.fn
+local cmd = vim.cmd
 
 local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
 
@@ -8,7 +9,8 @@ if fn.empty(fn.glob(install_path)) > 0 then
   execute 'packadd packer.nvim'
 end
 
-vim.cmd [[packadd packer.nvim]]
-vim.cmd 'autocmd BufWritePost plugins.lua PackerCompile'
+cmd [[packadd packer.nvim]]
+cmd 'autocmd BufWritePost plugins.lua PackerCompile'
 
 require('plugins')
+require('settings')
---
+++ nvim/lua/settings.lua
@@ -0,0 +1,49 @@
+local b = vim.bo
+local g = vim.o
+local w = vim.wo
+
+-- globals {
+g.autowrite = true
+g.backup = false
+g.completeopt = "menuone,noselect,preview"
+g.foldlevelstart = 10
+g.hidden = true
+g.hlsearch = false
+g.ignorecase = true
+g.inccommand = "split"
+g.mouse = "a"
+g.path = vim.o.path .. "**"
+g.scrolloff = 1
+g.shortmess = vim.o.shortmess .. "Ic"
+g.showmatch = true
+g.showmode = false
+g.sidescrolloff = 5
+g.smartcase = true
+g.splitbelow = true
+g.splitright = true
+g.termguicolors = true
+g.ttimeoutlen = 100
+g.wildignorecase = true
+g.wildmode = "longest:lastused"
+g.wrap = false
+g.writebackup = false
+-- g.wildchar = "<C-z>"
+-- }
+
+-- local to window {
+w.cursorline = true
+w.foldcolumn = "1"
+w.foldmethod = "syntax"
+w.number = true
+w.numberwidth = 3
+w.relativenumber = true
+w.signcolumn = "auto"
+-- }
+
+-- local to buffer {
+b.expandtab = true
+b.smartindent = true
+b.swapfile = false
+b.textwidth = 120
+b.undofile = true
+-- }
