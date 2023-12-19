45f7e46304070ea811d0df412ab068e9c0ad03bc
Alberto Maturano <alberto@maturano.mx>
2023-12-19 15:22:35 -0600

    backup

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,44 +1,40 @@
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
   -- installing lazy.nvim
-  vim.fn.system {
+  vim.fn.system({
     'git',
     'clone',
     '--filter=blob:none',
     'https://github.com/folke/lazy.nvim.git',
     '--branch=stable',
     lazypath,
-  }
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
-vim.g.mapleader = ' '
 
-require('lazy').setup {
+require('lazy').setup({
   spec = {
     { 'LazyVim/LazyVim', import = 'lazyvim.plugins', opts = { colorscheme = 'catppuccin-mocha' } },
-    { import = 'lazyvim.plugins.extras.lang.typescript' },
-    {
-      'neovim/nvim-lspconfig',
-      opts = {
-        servers = { eslint = {} },
-        setup = {
-          eslint = function()
-            require('lazyvim.util').on_attach(function(client)
-              if client.name == 'eslint' then
-                client.server_capabilities.documentFormattingProvider = true
-              elseif client.name == 'tsserver' then
-                client.server_capabilities.documentFormattingProvider = false
-              end
-            end)
-          end,
-        },
-      },
-    },
   },
   defaults = {
     lazy = false,
     version = false,
   },
   checker = { enabled = true }, -- check for plugin updates
-}
+  performance = {
+    rtp = {
+      -- disable some rtp plugins
+      disabled_plugins = {
+        "gzip",
+        -- "matchit",
+        -- "matchparen",
+        -- "netrwPlugin",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
+  },
+})
