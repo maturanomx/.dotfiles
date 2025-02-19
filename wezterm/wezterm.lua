6364a88c0ee5542a65a166720f0abe68a7fc11ea
Alberto Maturano <alberto@maturano.mx>
2025-02-19 13:01:53 -0600

    [wezterm] switch tabs. improve fonts and background

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,10 +6,9 @@ local config = wezterm.config_builder()
 config = {
 	color_scheme = "Catppuccin Mocha",
 	font = wezterm.font_with_fallback({
-		{ family = "Victor Mono" },
-		{ family = "Symbols Nerd Font Mono", scale = 0.85 },
+		"Victor Mono",
+		"Symbols Nerd Font Mono",
 	}),
-	font_size = 14,
 	hide_tab_bar_if_only_one_tab = true,
 	keys = {
 		{
@@ -35,13 +34,16 @@ config = {
 				)
 			end),
 		},
+		{ mods = "CMD|OPT", key = "LeftArrow", action = act.ActivateTabRelative(-1) },
+		{ mods = "CMD|OPT", key = "RightArrow", action = act.ActivateTabRelative(1) },
 	},
+	macos_window_background_blur = 15,
 	set_environment_variables = {
 		-- FIXME: already defined on `environment`
 		PATH = "/opt/homebrew/bin:" .. os.getenv("PATH"),
 	},
 	term = "xterm-kitty",
-	window_background_opacity = 0.97,
+	window_background_opacity = 0.9,
 	window_decorations = "RESIZE",
 }
 
