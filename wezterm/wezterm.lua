d99056ee5349b16408cfc952414a162a7baa9b26
Alberto Maturano <alberto@maturano.mx>
2026-01-09 13:38:48 -0600

    [term] test add information on wezterm tab bar

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,12 +1,14 @@
 local wezterm = require("wezterm")
 
+local HOME = os.getenv("HOME")
 local PATH = "/opt/homebrew/bin:/home/linuxbrew/.linuxbrew/bin:" .. os.getenv("PATH")
 local VAULT_NAME = "brainotes"
-local VAULT_PATH = os.getenv("HOME") .. "/projects/" .. VAULT_NAME
+local VAULT_PATH = HOME .. "/projects/" .. VAULT_NAME
 
 local act = wezterm.action
 local config = wezterm.config_builder()
 local lastWorkspace = nil
+local W_PADDING = 3
 
 config = {
 	adjust_window_size_when_changing_font_size = false,
@@ -66,8 +68,84 @@ config = {
 		PATH = PATH,
 	},
 	-- use_fancy_tab_bar = false,
-	window_background_opacity = 0.97,
+	window_background_opacity = 0.9,
 	window_decorations = "RESIZE",
+	window_padding = { bottom = W_PADDING, left = W_PADDING, right = W_PADDING, top = W_PADDING },
 }
+
+local function get_working_task()
+	local success, result, _ = wezterm.run_child_process({
+		"task",
+		"rc:" .. HOME .. "/.dotfiles/task/taskrc",
+		"rc.data.location:" .. HOME .. "/.private/task",
+		"rc.verbose:nothing",
+		"status:pending",
+		"priority+",
+		"-WAITING",
+		"+ACTIVE",
+		"export",
+	}, {
+		PATH = PATH,
+	})
+
+	if not success or not result then
+		return ""
+	end
+
+	local tasks = wezterm.json_parse(result)
+
+	if #tasks == 0 then
+		return ""
+	elseif #tasks == 1 then
+		return tasks[1].description
+	else
+		return tasks[1].description .. " (" .. #tasks - 1 .. "+)"
+	end
+end
+
+wezterm.on("update-status", function(window, pane)
+	local segments = {
+		get_working_task(),
+		pane:get_domain_name(),
+		window:active_workspace(),
+	}
+
+	local color_scheme = window:effective_config().resolved_palette
+	-- `wezterm.color.parse` returns a Color object, which can be lighten or darken (amongst other things)
+	local bg = wezterm.color.parse(color_scheme.background)
+	local fg = color_scheme.foreground
+
+	local gradient_to, gradient_from = bg, bg
+	gradient_from = gradient_to:lighten(0.2) -- gradient_to:darken(0.2)
+
+	local gradient = wezterm.color.gradient(
+		{
+			orientation = "Horizontal",
+			colors = { gradient_from, gradient_to },
+		},
+		#segments -- as many colors as segments
+	)
+
+	-- Build up the elements to send to `wezterm.format`
+	local elements = {}
+	local is_first = true
+
+	for i, seg in ipairs(segments) do
+		if seg and seg ~= "" then
+			if is_first then
+				table.insert(elements, { Background = { Color = "none" } })
+			end
+			table.insert(elements, { Foreground = { Color = gradient[i] } })
+			table.insert(elements, { Text = "" })
+			table.insert(elements, { Foreground = { Color = fg } })
+			table.insert(elements, { Background = { Color = gradient[i] } })
+			table.insert(elements, { Text = "  " .. seg .. "  " })
+
+			is_first = false
+		end
+	end
+
+	window:set_right_status(wezterm.format(elements))
+end)
 
 return config
